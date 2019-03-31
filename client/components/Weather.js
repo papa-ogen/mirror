90debcb3630ec2b7869595f226e8749a54bef213
Jörgen Thelin <thelin.jorgen@gmail.com>
2019-03-31 20:58:13 +0200

    Removed console

--- client/components/Weather.js
+++ client/components/Weather.js
@@ -11,7 +11,6 @@ export default function Weather() {
 
   useEffect(() => {
     socket.on('weather-response', payload => {
-      console.log('?', payload)
       setWeather(payload);
     });
   }, [weather]);
