b4dd6248dbe68270894533eec93269a913a57aa1
Jörgen Thelin <thelin.jorgen@gmail.com>
2019-03-31 20:28:04 +0200

    Added sockets and date-fns

--- client/components/Weather.js
+++ client/components/Weather.js
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import socket from '../socket';
+import { distanceInWords } from 'date-fns';
+import sv from 'date-fns/locale/sv'
+
+
 
 export default function Weather() {
   const [data, setData] = useState({});
@@ -16,10 +21,19 @@ export default function Weather() {
     return () => { ignore = true; }
   }, []);
 
+  function response(res) {
+    console.log('hello', res)
+  }
+
+  socket.on('weather-response', response)
+
+  const lastFetched = data.now ? distanceInWords(new Date(data.now), new Date(), { locale: sv }) : ''
   return (
     <section>
-      <h1>Weather</h1>
-      <pre>{console.log(data)}</pre>
+      <h1>Väder</h1>
+      <h3>{data.temp}°</h3>
+      <p>{data.weather}</p>
+      <p>Senast hämtad {lastFetched}</p>
     </section>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,33 @@ import { config as dotenv } from 'dotenv'
 import express from 'express';
 import cors from 'cors'
 import './lib/cron'
-import { getWeather } from './lib/weather'
-
-dotenv()
+import db from './lib/db';
+import cron from 'node-cron';
 
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+dotenv()
 
 app.use(cors());
 
 app.get(`/weather`, async (req, res, next) => {
-  const weather = await getWeather()
-  res.json(weather);
+  const { weather } = db.value();
+  res.json(weather[0]);
 });
 
-app.listen(process.env.PORT, () => {
+io.on('connection', () => {
+  console.log('a user is connected...')
+})
+
+cron.schedule(`5 6 * * *`, () => {
+  const { weather } = db.value();
+  console.log(`⏲️ RUNNING THE CRON SENDING WEATHER`);
+  io.emit('weather-response', weather[0]);
+});
+
+
+http.listen(process.env.PORT, () => {
   console.log(`Example App running on port http://localhost:${process.env.PORT}`);
 });
